e42ea95d17c5bd82c575cc4456b25b36b528ad87
Sebastiano Lanfranchi <sebastiano.lanfranchi@ff3d.com>
2022-07-18 15:38:26 +0200

    Feat: sidebar navbar

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
+import NavBar from '../NavBar';
+import SideBar from '../SideBar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   // Put Header or Footer Here
-  return <>{children}</>;
-}
+  return <>
+  <NavBar></NavBar>
+  <SideBar></SideBar>
+  {children}
+  </>;
+} 
